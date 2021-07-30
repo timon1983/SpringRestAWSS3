123c72a8ff1abeabddf26c1f9582949aa7fd8828
Timur <68236317+timon1983@users.noreply.github.com>
2021-07-30 16:24:16 +0300

    Update UserAuthRepository.java

--- src/main/java/com/example/SpringBootRestAWSS3/repository/UserAuthRepository.java
+++ src/main/java/com/example/SpringBootRestAWSS3/repository/UserAuthRepository.java
@@ -2,7 +2,6 @@ package com.example.SpringBootRestAWSS3.repository;
 
 import com.example.SpringBootRestAWSS3.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserAuthRepository extends JpaRepository<User, Long> {
